4867975e270e314734d7dc55124faedd37645261
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-08-11 21:04:10 +0530

    Update App.jsx

--- spotify-admin/src/App.jsx
+++ spotify-admin/src/App.jsx
@@ -10,7 +10,7 @@ import Sidebar from './components/Sidebar';
 import Navbar from './components/Navbar';
 
 
-export const url = 'http://localhost:4000 || https://spotify-backend-sand.vercel.app/'
+export const url = 'http://localhost:4000 || https://spotify-three-sable.vercel.app/'
 
 const App = () => {
   return (
