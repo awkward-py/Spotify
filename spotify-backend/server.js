a20e1342be9bbabbc46617cb50c8ad26d1ff6ca8
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-08-12 17:15:03 +0530

    Update server.js

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -1,23 +1,23 @@
-import express from 'express'
-import cors from  'cors'
-import 'dotenv/config'
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+import path from 'path'; // Import path module for resolving paths
 import songRouter from './src/routes/songRoute.js';
 import connectDB from './src/config/mongodb.js';
 import connectCloudinary from './src/config/cloudinary.js';
-import albumRouter from './src/routes/albumRoute.js';
-
 
 // app config
-
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
 // middlewares
-
 app.use(express.json());
 app.use(cors());
+
+// Serve static files from the 'public' directory
+app.use('/assets', express.static(path.join(path.resolve(), 'public/assets')));
 
 
 
