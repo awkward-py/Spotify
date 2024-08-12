193e711ed9c52432416fda006c47491b761ac63e
Pankaj Rawat <116781641+awkward-py@users.noreply.github.com>
2024-08-12 17:17:48 +0530

    Update server.js

--- spotify-backend/server.js
+++ spotify-backend/server.js
@@ -1,23 +1,23 @@
-import express from 'express';
-import cors from 'cors';
-import 'dotenv/config';
-import path from 'path'; // Import path module for resolving paths
+ import express from 'express'
+import cors from  'cors'
+import 'dotenv/config'
 import songRouter from './src/routes/songRoute.js';
 import connectDB from './src/config/mongodb.js';
 import connectCloudinary from './src/config/cloudinary.js';
+import albumRouter from './src/routes/albumRoute.js';
+
 
 // app config
+
 const app = express();
 const port = process.env.PORT || 4000;
 connectDB();
 connectCloudinary();
 
 // middlewares
+
 app.use(express.json());
 app.use(cors());
-
-// Serve static files from the 'public' directory
-app.use('/assets', express.static(path.join(path.resolve(), 'public/assets')));
 
 
 
@@ -28,4 +28,4 @@ app.use('/api/album',albumRouter)
 
 app.get('/',(req,res)=>res.send("API Working messeage from Pankaj Rawat"))
 
-app.listen(port,()=>console.log(`server started on ${port}`))
+app.listen(port,()=>console.log(server started on ${port}))
